923941325ee77a402dea7f39c86d713838a87225
heronhoga <nozzeravixer@gmail.com>
2025-04-28 04:54:32 +0700

    feat:set create memo func

--- src/apis/memo.ts
+++ src/apis/memo.ts
@@ -2,3 +2,27 @@ import axios, { AxiosError } from "axios";
 
 const BASE_URL = import.meta.env.BACKEND_PORT;
 const APP_KEY = import.meta.env.APP_KEY;
+
+interface MemoData {
+  date: string;
+  note: string;
+}
+
+interface CreateMemoResponse {
+  message: string;
+}
+export async function createMemo(data: MemoData, token: string): Promise<CreateMemoResponse> {
+  try {
+    const response = await axios.post(`${BASE_URL}/memo/create`, data, {
+      headers: {
+        hgtoken: APP_KEY,
+        Authorization: `Bearer ${token}` 
+      },
+    });
+    return response.data;
+  } catch (error: unknown) {
+    const axiosError = error as AxiosError<{ message: string }>;
+    console.log(axiosError);
+    throw axiosError.response?.data || { message: "Something went wrong" };
+  }
+}
