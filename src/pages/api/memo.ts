424b08e323d37c1a83b2a23ff0a02dfd7058a19d
heronhoga <nozzeravixer@gmail.com>
2025-04-28 21:52:40 +0700

    feat:add create memo backend proxy

---
+++ src/pages/api/memo.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from "astro";
+import { createMemo } from "../../apis/memo";
+
+export const POST: APIRoute = async ({request}) => {
+    const data = await request.json()
+    const token = request.headers.get("memoraire_token") || ""
+    try {
+        const result = await createMemo(data, token)
+        return new Response(JSON.stringify(result), { status: 200 });
+    } catch (error:any) {
+        return new Response(JSON.stringify({ message: error.message }), {
+            status: 400,
+          });
+    }
+}
