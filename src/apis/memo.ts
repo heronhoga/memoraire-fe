22f824c411cc07dc519c4e5533499de3c97ee587
heronhoga <nozzeravixer@gmail.com>
2025-04-30 01:36:54 +0700

    feat:integrate fetch memo data

--- src/apis/memo.ts
+++ src/apis/memo.ts
@@ -11,14 +11,45 @@ interface MemoData {
 interface CreateMemoResponse {
   message: string;
 }
-export async function createMemo(data: MemoData, token: string): Promise<CreateMemoResponse> {
+
+interface GetMemoResponse {
+  message: string;
+  data: unknown;
+}
+
+//create memo
+export async function createMemo(
+  data: MemoData,
+  token: string
+): Promise<CreateMemoResponse> {
   try {
     const response = await axios.post(`${BASE_URL}/memo/create`, data, {
       headers: {
         hgtoken: APP_KEY,
-        Authorization: `Bearer ${token}` 
+        Authorization: `Bearer ${token}`,
       },
     });
+    return response.data;
+  } catch (error: unknown) {
+    const axiosError = error as AxiosError<{ message: string }>;
+    console.log(axiosError);
+    throw axiosError.response?.data || { message: "Something went wrong" };
+  }
+}
+
+//get memo
+export async function getMemo(
+  token: string,
+  page: number
+): Promise<GetMemoResponse> {
+  try {
+    const response = await axios.get(`${BASE_URL}/memo?page=${page}`, {
+      headers: {
+        hgtoken: APP_KEY,
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     return response.data;
   } catch (error: unknown) {
     const axiosError = error as AxiosError<{ message: string }>;
--- src/pages/api/memo.ts
+++ src/pages/api/memo.ts
@@ -1,18 +1,34 @@
 import type { APIRoute } from "astro";
-import { createMemo } from "../../apis/memo";
+import { createMemo, getMemo } from "../../apis/memo";
 
-export const POST: APIRoute = async ({request}) => {
-    const data = await request.json()
-    const token = request.headers.get("memoraire_token") || ""
-    try {
-        const result = await createMemo(data, token)
-        return new Response(JSON.stringify(result), { status: 200 });
-    } catch (error:any) {
-        console.log("inilah errornya dari proxy backend", error)
-        return new Response(JSON.stringify({ message: error.message }), {
-            status: 400,
-          });
-    }
-}
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.json();
+  const token = request.headers.get("memoraire_token") || "";
+  try {
+    const result = await createMemo(data, token);
+    return new Response(JSON.stringify(result), { status: 200 });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ message: error.message }), {
+      status: 400,
+    });
+  }
+};
 
-export const prerender = false
+export const GET: APIRoute = async ({ request }) => {
+  const token = request.headers.get("memoraire_token") || "";
+
+  const url = new URL(request.url);
+  const pageParam = url.searchParams.get("page");
+  const page = parseInt(pageParam || "1", 10);
+
+  try {
+    const result = await getMemo(token, page);
+    return new Response(JSON.stringify(result), { status: 200 });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ message: error.message }), {
+      status: 400,
+    });
+  }
+};
+
+export const prerender = false;
