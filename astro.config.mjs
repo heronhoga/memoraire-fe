7cef38d50256d46bd67676617ab21ed1fd41721d
heronhoga <nozzeravixer@gmail.com>
2025-05-06 22:20:38 +0700

    fix:fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  server: {
+    port: 3000,
+    host: true,
+  },
   vite: {
     plugins: [tailwindcss()]
   },
